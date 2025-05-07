97ebf856305b211b07bd72adf5f061927416c3b1
DaffTzy5912 <dafanation999@gmail.com>
2025-05-07 12:47:04 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'khodam-app-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
